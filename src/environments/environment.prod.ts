cf1627f3e8b8e3d89d8233504e52c22cf32295e7
Vinicius <48769407+ViniciusCoelhoNascimento@users.noreply.github.com>
2025-03-17 21:43:03 -0300

    rename

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    apiURL: 'http://localhost:8080/',
+    firebase: {
+        apiKey: "AIzaSyCudYlx6ZQe2sHrMR6IRj5h0WhrEoe61s8",
+        authDomain: "ta-na-hora-3dfaa.firebaseapp.com",
+        projectId: "ta-na-hora-3dfaa",
+        storageBucket: "ta-na-hora-3dfaa.firebasestorage.app",
+        messagingSenderId: "843933765524",
+        appId: "1:843933765524:web:22fd495976a288d8ca4981",
+        measurementId: "G-727W399V9J",
+        vapidKey: "BPPBr_jLY_Rlm2pe36OY7SKLNFxw1s9jEK89IpdcDTXTUpMgqoj2c3OCJYDqd4YkQKeOr-LXyfpI8XTkZ4HqFqE"
+    }
+};
