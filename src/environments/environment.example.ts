2e9ef74ea13e9ae56d5900a8147fb9fcd66bb0a2
Vinicius <48769407+ViniciusCoelhoNascimento@users.noreply.github.com>
2025-04-14 21:40:56 -0300

    chore: environmnet.example.ts

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: false,
+    apiURL: 'http://localhost:8080/',
+    firebase: {
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: "",
+        vapidKey: ""
+    }
+};
